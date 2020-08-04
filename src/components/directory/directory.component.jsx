ad5ca00dd683a090c370aefc3287c047817c7811
giocare <jessica.adjei102@gmail.com>
2020-08-04 18:54:19 -0400

    Integrated Firebase + Created Sign In Page + Sign In With Google Button

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import MenuItem from '../menu-item.component.jsx/menu-item.component';
+import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss'
 // needs to be a class because we need a state
