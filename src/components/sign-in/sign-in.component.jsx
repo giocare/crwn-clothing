a3683a28108f325a406ad607c974ac48618f45b6
giocare <jessica.adjei102@gmail.com>
2020-08-04 21:12:57 -0400

    Finished Sign In - Handle Submit Method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 
 // class component becuz we have to store what the user is typing in
@@ -17,10 +17,18 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: ''})
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: ''})
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 
     handleChange = event => {
