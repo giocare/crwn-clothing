d920fd4e2aad18fc93b44aacecf5048e3b92fc8a
giocare <jessica.adjei102@gmail.com>
2020-08-08 13:36:04 -0400

    Added AddToCart Button To Items + Styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './collection-item.styles.scss'
 import CollectionPreview from '../collection-preview/collection-preview.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 
 // functional component
@@ -18,6 +19,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton>Add To Cart</CustomButton>
     </div>
 );
 
